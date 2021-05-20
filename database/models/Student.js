5c8a092d9dff98c8ecfd65d6343767b8cbe94920
jamyooes <james.yoo715@gmail.com>
2021-05-19 21:28:54 -0400

    spacing

--- database/models/Student.js
+++ database/models/Student.js
@@ -1,34 +1,32 @@
-const Sequelize = require('sequelize');
-const db = require('../db');
+const Sequelize = require("sequelize");
+const db = require("../db");
 
 const Student = db.define("student", {
-
   firstname: {
     type: Sequelize.STRING,
-    allowNull: false
+    allowNull: false,
   },
 
   lastname: {
     type: Sequelize.STRING,
-    allowNull: false
+    allowNull: false,
   },
 
   email: {
     type: Sequelize.STRING,
-    allowNull: false
+    allowNull: false,
   },
 
-  imageUrl:{
+  imageUrl: {
     type: Sequelize.STRING,
-    defaultValue: ""
+    defaultValue: "",
   },
 
-  gpa:{
-  type: Sequelize.FLOAT,
-  min: 0.0,
-  max: 4.0
-  }
-
+  gpa: {
+    type: Sequelize.FLOAT,
+    min: 0.0,
+    max: 4.0,
+  },
 });
 
 module.exports = Student;
